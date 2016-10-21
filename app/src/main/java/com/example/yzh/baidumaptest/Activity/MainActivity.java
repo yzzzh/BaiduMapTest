e432759dc8815b9e7855973b9bc5cd8352e20119
YZH <453505647@qq.com>
2016-10-21 23:41:11 +0800

    第五部分
    对大部分功能进行了完善，还有一些bug待修改。

--- app/src/main/java/com/example/yzh/baidumaptest/Activity/MainActivity.java
+++ app/src/main/java/com/example/yzh/baidumaptest/Activity/MainActivity.java
@@ -7,14 +7,21 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -30,14 +37,19 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.PolylineOptions;
+import com.baidu.mapapi.map.TextOptions;
 import com.baidu.mapapi.model.LatLng;
 import com.example.yzh.baidumaptest.R;
 import com.example.yzh.baidumaptest.database.RadarDB;
 import com.example.yzh.baidumaptest.model.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.Bmob;
+
+import static com.baidu.mapapi.BMapManager.getContext;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
@@ -56,12 +68,14 @@ public class MainActivity extends Activity implements View.OnClickListener{
     //朋友/敌人的坐标点
     private LatLng point;
     //覆盖物图片
-    private BitmapDescriptor bitmap;
+    private Bitmap bitmapFriend;
+    private Bitmap bitmapEnermy;
     //组件
     private Button btnLocate;
     private Button btnSendMessage;
     private Button btnFriend;
     private Button btnEnermy;
+    private ImageView image;
     //数据库
     private RadarDB db;
     //朋友列表
@@ -91,6 +105,38 @@ public class MainActivity extends Activity implements View.OnClickListener{
         //初始化数据库
         db = RadarDB.getInstance(this);
 
+        Person person1 = new Person();
+        person1.setName("尹泽豪");
+        person1.setNumber("13602955218");
+        person1.setType("friend");
+        person1.setLatitude("22.20");
+        person1.setLongitude("113.48");
+        db.addPerson(person1);
+
+        Person person2 = new Person();
+        person2.setName("吴国鹏");
+        person2.setNumber("15820578134");
+        person2.setType("enermy");
+        person2.setLatitude("22.29");
+        person2.setLongitude("113.49");
+        db.addPerson(person2);
+
+        Person person3 = new Person();
+        person3.setName("戴鹏");
+        person3.setNumber("13143126140");
+        person3.setType("friend");
+        person3.setLatitude("22.32");
+        person3.setLongitude("113.51");
+        db.addPerson(person3);
+
+        Person person4 = new Person();
+        person4.setName("骚猪");
+        person4.setNumber("13360787823");
+        person4.setType("enermy");
+        person4.setLatitude("22.30");
+        person4.setLongitude("113.58");
+        db.addPerson(person4);
+
         //初始化组件
         initViews();
         //加载朋友/敌人
@@ -114,9 +160,6 @@ public class MainActivity extends Activity implements View.OnClickListener{
         //开始定位
         locationClient.start();
 
-        //获取覆盖物图片
-        bitmap = BitmapDescriptorFactory.fromResource(R.drawable.logo);
-
         //注册监听器
         registerRec();
     }
@@ -126,6 +169,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         switch (v.getId()){
             case R.id.btnSendMessage:
                 warning();
+//                rotate();
                 break;
             case R.id.btnLocate:
                 setCenter();
@@ -193,6 +237,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         btnEnermy.setOnClickListener(this);
         btnLocate.setOnClickListener(this);
         btnSendMessage.setOnClickListener(this);
+        image = (ImageView) findViewById(R.id.imageview_sweep);
     }
 
     //定位的相关设置
@@ -300,6 +345,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                rotate();
                 sendMessage();
             }
         });
@@ -321,6 +367,28 @@ public class MainActivity extends Activity implements View.OnClickListener{
         alertDialog.show();
     }
 
+    //获取两点之间的距离
+    public double getDistance(LatLng point1,LatLng point2) {
+
+        final double R = 6370996.81;  //地球的半径
+
+        double lat1 = point1.latitude;
+        double lng1 = point1.longitude;
+        double lat2 = point2.latitude;
+        double lng2 = point2.longitude;
+
+    /*
+     * 获取两点间x,y轴之间的距离
+     */
+        double x = (lng2 - lng1)*Math.PI*R*Math.cos(((lat1+lat2)/2)*Math.PI/180)/180;
+        double y = (lat2 - lat1)*Math.PI*R/180;
+
+
+        double distance = Math.hypot(x, y);   //得到两点之间的直线距离
+
+        return   distance;
+    }
+
     //设置自己的坐标点到屏幕中心
     private void setCenter(){
         mapStatusUpdate= MapStatusUpdateFactory.newLatLngZoom(myPoint, 16);   //设置地图中心点以及缩放级别
@@ -329,30 +397,140 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     //描绘朋友/敌人的坐标
     private void drawPositon(){
+
         double latitude;
         double logitude;
 
+        View bitmapFriendView;
+        View bitmapEnermyView;
+
+        TextView friendName;
+        TextView friendNumber;
+
+        TextView enermyName;
+        TextView enermyNumber;
+
+        double textLat;
+        double textLng;
+        String distance;
+        LatLng llText;
+
+        OverlayOptions options;
+        OverlayOptions ooPolyline;
+        OverlayOptions textOption;
+
         for (int i = 0;i < friendList.size();i++){
             if (friendList.get(i).getLatitude() != null && friendList.get(i).getLongitude() != null) {
+
+                bitmapFriendView = LayoutInflater.from(this).inflate(R.layout.bitmap_friend, null);
+                friendName = (TextView) bitmapFriendView.findViewById(R.id.bitmap_friend_name);
+                friendNumber = (TextView) bitmapFriendView.findViewById(R.id.bitmap_friend_number);
+                friendName.setText(friendList.get(i).getName());
+                friendNumber.setText(friendList.get(i).getNumber());
+
+                bitmapFriend  = getViewBitmap(bitmapFriendView);
+
                 latitude = Double.parseDouble(friendList.get(i).getLatitude());
                 logitude = Double.parseDouble(friendList.get(i).getLongitude());
-
                 point = new LatLng(latitude, logitude);
-                OverlayOptions options = new MarkerOptions().position(point).icon(bitmap).draggable(true);
+
+                options = new MarkerOptions()
+                        .position(point)
+                        .icon(BitmapDescriptorFactory.fromBitmap(bitmapFriend))
+                        .draggable(true);
                 baiduMap.addOverlay(options);
+
+                //描绘两点之间的直线
+                List<LatLng> points = new ArrayList<LatLng>();
+                points.add(point);
+                points.add(myPoint);
+                ooPolyline = new PolylineOptions().width(10).color(0xFF3988B5).points(points);
+                baiduMap.addOverlay(ooPolyline);
+
+                //描绘两点之间的直线距离
+                textLat = (point.latitude + myPoint.latitude) / 2;
+                textLng = (point.longitude + myPoint.longitude) / 2;
+                distance = String.format("%.2f km",getDistance(point,myPoint) / 1000);
+                llText = new LatLng(textLat,textLng);
+                textOption = new TextOptions()
+                        .text(distance)
+                        .fontSize(40)
+                        .fontColor(0xFFFF00FF)
+                        .text(distance)
+                        .rotate(-30)
+                        .position(llText);
+                baiduMap.addOverlay(textOption);
+
             }
         }
         for (int i = 0;i < enermyList.size();i++){
-            if (friendList.get(i).getLatitude() != null && friendList.get(i).getLongitude() != null) {
+            if (enermyList.get(i).getLatitude() != null && enermyList.get(i).getLongitude() != null) {
+
+                bitmapEnermyView = LayoutInflater.from(this).inflate(R.layout.bitmap_enermy, null);
+                enermyName = (TextView) bitmapEnermyView.findViewById(R.id.bitmap_enermy_name);
+                enermyNumber = (TextView) bitmapEnermyView.findViewById(R.id.bitmap_enermy_number);
+                enermyName.setText(enermyList.get(i).getName());
+                enermyNumber.setText(enermyList.get(i).getNumber());
+
+                bitmapEnermy = getViewBitmap(bitmapEnermyView);
+
                 latitude = Double.parseDouble(enermyList.get(i).getLatitude());
                 logitude = Double.parseDouble(enermyList.get(i).getLongitude());
 
                 point = new LatLng(latitude, logitude);
-                OverlayOptions options = new MarkerOptions().position(point).icon(bitmap).draggable(true);
+                options = new MarkerOptions()
+                        .position(point)
+                        .icon(BitmapDescriptorFactory.fromBitmap(bitmapEnermy))
+                        .draggable(true);
                 baiduMap.addOverlay(options);
+
+                List<LatLng> points = new ArrayList<LatLng>();
+                points.add(point);
+                points.add(myPoint);
+                ooPolyline = new PolylineOptions().width(10).color(0xFF3988B5).points(points);
+                baiduMap.addOverlay(ooPolyline);
+
+                textLat = (point.latitude + myPoint.latitude) / 2;
+                textLng = (point.longitude + myPoint.longitude) / 2;
+                distance = String.format("%.2f km",getDistance(point,myPoint) / 1000);
+                llText = new LatLng(textLat,textLng);
+                textOption = new TextOptions()
+                        .text(distance)
+                        .fontSize(40)
+                        .fontColor(0xFFFF00FF)
+                        .text(distance)
+                        .rotate(-30)
+                        .position(llText);
+                baiduMap.addOverlay(textOption);
             }
         }
     }
+
+    //将自定义的布局转化为bitmap对象
+    private Bitmap getViewBitmap(View addViewContent) {
+
+        addViewContent.setDrawingCacheEnabled(true);
+
+        addViewContent.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        addViewContent.layout(0, 0,
+                addViewContent.getMeasuredWidth(),
+                addViewContent.getMeasuredHeight());
+
+        addViewContent.buildDrawingCache();
+        Bitmap cacheBitmap = addViewContent.getDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(cacheBitmap);
+
+        return bitmap;
+    }
+
+    //旋转动画
+    private void rotate(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.rotate_indefinitely);
+        image.startAnimation(animation);
+    }
+
 
     //监听是否发送成功
     public class SmsStatusReceiver extends BroadcastReceiver {
